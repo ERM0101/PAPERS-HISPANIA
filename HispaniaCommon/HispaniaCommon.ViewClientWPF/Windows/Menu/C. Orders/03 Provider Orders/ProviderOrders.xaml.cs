5b31717af85054086b6bb9ed3c13e494e3e3074b
Aníbal Díaz Silva <adiaz@iodigital.es>
2023-07-03 12:22:30 +0200

    Gestió de Comandes De Proveidor 1.- Quitar botones con acciones de albaranes. 2.- Del fichero que se extrae al hacer click en el botón “Crear Excel”…    2.1.- Quitar los campos marcados en rojo:

--- HispaniaCommon/HispaniaCommon.ViewClientWPF/Windows/Menu/C. Orders/03 Provider Orders/ProviderOrders.xaml.cs
+++ HispaniaCommon/HispaniaCommon.ViewClientWPF/Windows/Menu/C. Orders/03 Provider Orders/ProviderOrders.xaml.cs
@@ -637,8 +637,9 @@ namespace HispaniaCommon.ViewClientWPF.Windows
                 btnPrint.Click += BtnPrint_Click;
                 btnSendByEmail.Click += BtnSendByEMail_Click;
                 btnCreateBill.Click += BtnCreateBill_Click;
-                btnCreateDeliveryNote.Click += BtnCreateDeliveryNote_Click;
-                btnSplitProviderOrder.Click += BtnSplitProviderOrder_Click;
+                //TODO: PH03072023
+                //btnCreateDeliveryNote.Click += BtnCreateDeliveryNote_Click;
+                //btnSplitProviderOrder.Click += BtnSplitProviderOrder_Click;
                 btnRefresh.Click += BtnRefresh_Click;
                 btnChangeDate.Click += BtnChangeDate_Click;
                 btnProforma.Click += BtnProformaPrint_Click;
